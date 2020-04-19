14c5ed7ac9768638dd44f870fb674d4e2a32e415
sundarasswamy <sundaraswamy@gmail.com>
2020-04-19 09:15:14 +0530

    Updated Material Information

--- src/app/edit-matinfos/edit-matinfos.component.ts
+++ src/app/edit-matinfos/edit-matinfos.component.ts
@@ -25,7 +25,8 @@ export class EditMatinfosComponent implements OnInit {
   ngOnInit(): void {
 
     this.materialInfoForm = this.formBuilder.group({
-
+      _id: ['', Validators.required],
+      newOrEdit: 'Edit',
       numberOfSpecs: ['', Validators.required],
       materialCatType : ['', Validators.required],
       materialCatDept:  ['', Validators.required],
@@ -45,6 +46,7 @@ export class EditMatinfosComponent implements OnInit {
       if (this.t.length < this.numberOfSpecs) {
         for (let i = this.t.length; i < this.numberOfSpecs; i++) {
           this.t.push(this.formBuilder.group({
+            _id: '',
             label: ['', Validators.required],
             datatype: ['', Validators.required],
             dropValues : [''],
@@ -58,6 +60,8 @@ export class EditMatinfosComponent implements OnInit {
       }
 
       this.materialInfoForm.setValue({
+        _id : data._id,
+        newOrEdit : 'Edit',
         numberOfSpecs: data.numberOfSpecs,
         materialCatType: data.materialCatType,
         materialCatDept: data.materialCatDept,
@@ -76,6 +80,7 @@ export class EditMatinfosComponent implements OnInit {
     if (this.t.length < numberOfSpecs) {
       for (let i = this.t.length; i < numberOfSpecs; i++) {
         this.t.push(this.formBuilder.group({
+          _id: '',
           label: ['', Validators.required],
           datatype: ['', Validators.required],
           dropValues : [''],
@@ -92,22 +97,49 @@ export class EditMatinfosComponent implements OnInit {
   onFormSubmit() {
     this.isLoadingResults = true;
     const obj = JSON.parse(JSON.stringify(this.materialInfoForm.value, null, 4));
+    console.log(obj);
     for ( let i = obj.additionalInfo.length - 1; i >= 0 ; i--) {
-      const dropValues =  obj.additionalInfo[i].dropValues;
+      const dropValues =  obj.additionalInfo[i].dropValues.toString();
+      if (obj.additionalInfo[i]._id  === '') {
+            delete obj.additionalInfo[i]._id;
+      }
       const nameArr = dropValues.split(',');
       obj.additionalInfo[i].dropValues = nameArr;
 
     }
-    this.api.updateMaterialInfo(this.id, obj)
-      .subscribe((res: any) => {
+
+    if (obj.newOrEdit === 'Edit') {
+      delete obj.newOrEdit;
+      this.api.updateMaterialInfo(this.id, obj)
+        .subscribe((res: any) => {
+            const id = res._id;
+            this.isLoadingResults = false;
+            this.router.navigate(['/show-matInfo', id]);
+          }, (err: any) => {
+            console.log(err);
+            this.isLoadingResults = false;
+          }
+        );
+    }
+    if (obj.newOrEdit === 'New') {
+      delete obj.newOrEdit;
+      delete obj._id;
+
+      for ( let i = obj.additionalInfo.length - 1; i >= 0 ; i--) {
+        const dropValues =  obj.additionalInfo[i].dropValues.toString();
+        delete obj.additionalInfo[i]._id;
+      }
+      this.api.addMaterialInfo(obj)
+        .subscribe((res: any) => {
           const id = res._id;
           this.isLoadingResults = false;
-          this.router.navigate(['/show-article', id]);
+          this.router.navigate(['/show-matInfo', id]);
         }, (err: any) => {
           console.log(err);
           this.isLoadingResults = false;
-        }
-      );
+        });
+
+    }
   }
 
 }
